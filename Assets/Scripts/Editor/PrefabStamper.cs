e0ec3d2cc472bdd780df3e3a887a79b4ed5bb5b9
unitycoder <support@unitycoder.com>
2017-09-13 21:09:21 +0800

    add enabled toggle, add current stamp name, add bounds to json, add wirecircle preview paint area (not good yet), move stamp classes to own files, cleanup and rename methods

--- Assets/Scripts/Editor/PrefabStamper.cs
+++ Assets/Scripts/Editor/PrefabStamper.cs
@@ -1,4 +1,5 @@
 // PrefabStamper by http://www.eastshade.com/
+// modified by unitycoder.com
 
 using UnityEngine;
 using UnityEditor;
@@ -9,7 +10,7 @@ namespace EastshadeStudio
 {
     public class PrefabStamper : EditorWindow
     {
-        static LayerMask targetLayer = 0 << 1;
+        static LayerMask targetLayer = 0 << 8;
 
         const string stampDataFolder = "/Text/Stamps/";
 
@@ -24,6 +25,243 @@ namespace EastshadeStudio
         float smallest = .85f;
         float largest = 1.15f;
         static PrefabStamper instance;
+
+        static int undoGroupIncrementer = 0;
+        private float radius;
+        static bool isEnabled = true;
+        static string currentStampName = "";
+
+        [MenuItem("Assets/Stamp")]
+        static void StartStamping()
+        {
+            Init();
+            Object obj = Selection.activeObject;
+            if (obj.GetType() == typeof(TextAsset))
+            {
+                // instance.Stamp = Serializer.Deserialize<StampData>((Selection.activeObject as TextAsset).text); // EasyJson plugin
+                instance.Stamp = JsonUtility.FromJson<StampData>((Selection.activeObject as TextAsset).text);
+            } else
+            { // we will generate a stamp from this single prefab
+                StampData singleStamp = new StampData();
+                StampChild singleChild = new StampChild();
+                singleChild.PrefabPath = AssetDatabase.GetAssetPath(obj);
+                singleStamp.prefabs.Add(singleChild);
+                instance.Stamp = singleStamp;
+            }
+            currentStampName = Selection.activeObject.name;
+        }
+
+        // check if menu can be enabled for this selection or not
+        [MenuItem("Assets/Stamp", true)]
+        static bool StampCheck()
+        {
+            Object candidate = Selection.activeObject;
+
+            if (candidate == null)
+            {
+                return false;
+            }
+
+            if (Selection.activeObject.GetType() == typeof(GameObject))
+            {
+                return true;
+            }
+
+            if (Selection.activeObject.GetType() == typeof(TextAsset))
+            {
+                try
+                {
+                    // if (Serializer.Deserialize<StampData>((candidate as TextAsset).text) == null) // EasyJson plugin
+                    if (JsonUtility.FromJson((candidate as TextAsset).text, typeof(StampData)) == null)
+                    {
+                        return false;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void Init()
+        {
+            if (LayerMask.GetMask("Terrain") > 0)
+            {
+                targetLayer = LayerMask.NameToLayer("Terrain");
+            }
+
+            // Get existing open window or if none, make a new one:
+            PrefabStamper window = (PrefabStamper)GetWindow(typeof(PrefabStamper));
+            window.Show();
+            window.titleContent.text = "PrefabStamper";
+            window.minSize = new Vector2(248, 168);
+            window.maxSize = new Vector2(250, 170);
+            instance = window;
+        }
+
+        void OnEnable()
+        {
+            SceneView.onSceneGUIDelegate += OnScene;
+        }
+        void OnDisable()
+        {
+            SceneView.onSceneGUIDelegate -= OnScene;
+        }
+
+        void OnGUI()
+        {
+            if (instance == null)
+            {
+                instance = (PrefabStamper)GetWindow(typeof(PrefabStamper));
+            }
+
+            isEnabled = EditorGUILayout.Toggle("Enabled", isEnabled);
+
+            EditorGUILayout.LabelField("Current stamp", currentStampName);
+
+            targetLayer = EditorGUILayout.LayerField("Target layer", targetLayer);
+
+            useNormal = EditorGUILayout.Toggle("Use Normal", useNormal);
+            randomYRot = EditorGUILayout.Toggle("Randomize Y Rotation", randomYRot);
+
+            sizeVariation = EditorGUILayout.Toggle("Randomize Scale", sizeVariation);
+            if (sizeVariation == true)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Smallest/Largest", GUILayout.MaxWidth(120));
+                smallest = EditorGUILayout.FloatField(smallest);
+                largest = EditorGUILayout.FloatField(largest);
+                EditorGUILayout.EndHorizontal();
+            }
+
+            if (GUILayout.Button("Done", GUILayout.MinHeight(30)))
+            {
+                instance.Close();
+            }
+        }
+
+        // main loop
+        static void OnScene(SceneView sceneview)
+        {
+            if (isEnabled == false) return;
+
+            if (Event.current.alt) // lat key is down
+            {
+                return;
+            }
+
+            if (instance == null)
+            {
+                instance = (PrefabStamper)GetWindow(typeof(PrefabStamper));
+            }
+
+            if (instance.Stamp == null) return;
+
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+            {
+                PaintStamp();
+                Event.current.Use();
+            }
+
+            if (Event.current.type == EventType.MouseUp)
+            {
+                instance.stampedTransforms.Clear();
+            }
+
+            if (instance.stampedTransforms.Count > 0)
+            {
+                UpdateStamper();
+            }
+
+            if (Event.current.type == EventType.Layout)
+            { // stop selecting objects
+                HandleUtility.AddDefaultControl(0);
+            }
+        }
+
+        static void UpdateStamper()
+        {
+            // rotate with scrollwheel
+            float scroll = (Event.current.type == EventType.scrollWheel) ? Event.current.delta.y * 4f : 0f;
+            if (Event.current.type == EventType.scrollWheel)
+            {
+                Event.current.Use();
+            }
+            foreach (Transform t in instance.stampedTransforms)
+            {
+                t.RotateAround(instance.lastHitPoint, instance.lastHitNormal, scroll);
+            }
+
+            Vector2 guiPosition = Event.current.mousePosition;
+            Ray ray = HandleUtility.GUIPointToWorldRay(guiPosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << targetLayer))
+            {
+                foreach (Transform t in instance.stampedTransforms)
+                {
+                    t.position += hit.point - instance.lastHitPoint;
+                }
+
+                instance.lastHitPoint = hit.point;
+                instance.lastHitNormal = hit.normal;
+
+                // preview circle
+                Handles.color = Color.blue;
+                Handles.DrawWireDisc(hit.point, hit.normal, instance.Stamp.bounds.extents.magnitude * 0.5f);
+                //Handles.DrawWireCube(hit.point, instance.Stamp.bounds.size);
+                HandleUtility.Repaint();
+            }
+            DropToTerrain(instance.stampedTransforms.ToArray());
+        }
+
+
+        static void PaintStamp()
+        {
+            Undo.IncrementCurrentGroup();
+
+            GameObject newStampsGroup = GameObject.Find("new stamps");
+            if (newStampsGroup == null || newStampsGroup.transform.root != newStampsGroup.transform)
+            {
+                newStampsGroup = new GameObject("new stamps");
+            }
+
+            Vector2 guiPosition = Event.current.mousePosition;
+            Ray ray = HandleUtility.GUIPointToWorldRay(guiPosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << targetLayer) == true)
+            {
+                instance.lastHitPoint = hit.point;
+                instance.lastHitNormal = hit.normal;
+                instance.stampedTransforms.Clear();
+
+                foreach (StampChild child in instance.Stamp.prefabs)
+                {
+                    var obj = AssetDatabase.LoadAssetAtPath(child.PrefabPath, typeof(GameObject));
+                    GameObject newGo = PrefabUtility.InstantiatePrefab(obj) as GameObject;
+                    //newGo.name = newGo.name.Split('(')[0];
+                    newGo.transform.parent = newStampsGroup.transform;
+                    newGo.transform.position = (child.position - instance.Stamp.center) + hit.point;
+                    newGo.transform.eulerAngles = child.eulerAngles;
+                    newGo.transform.localScale = child.scale;
+
+                    if (instance.sizeVariation == true)
+                    {
+                        newGo.transform.localScale *= Random.Range(instance.smallest, instance.largest);
+                    }
+
+                    instance.stampedTransforms.Add(newGo.transform);
+                    Undo.RegisterCreatedObjectUndo(newGo, "lay stamp" + undoGroupIncrementer.ToString());
+                }
+
+                undoGroupIncrementer++;
+            }
+        }
 
         [MenuItem("Tools/LevelEditTools/Prefab Stamper/Drop Selection to Terrain", false, 2)]
         static void DropSelectedToTerrain()
@@ -86,22 +324,28 @@ namespace EastshadeStudio
 
             StampData stampData = new StampData();
             Vector3 center = Vector3.zero;
+            Bounds bounds = new Bounds();
 
             foreach (GameObject go in Selection.gameObjects)
             {
-
                 if (!go.activeInHierarchy) continue;
-
                 StampChild sc = new StampChild();
                 sc.PrefabPath = AssetDatabase.GetAssetPath(PrefabUtility.GetPrefabParent(go));
                 sc.position = go.transform.position;
                 sc.eulerAngles = go.transform.eulerAngles;
                 sc.scale = go.transform.localScale;
                 center += sc.position;
+                // get bounds from all children
+                foreach (Transform child in go.transform)
+                {
+                    var mf = child.GetComponent<MeshFilter>();
+                    bounds.Encapsulate(mf.sharedMesh.bounds);
+                }
                 stampData.prefabs.Add(sc);
             }
 
             stampData.center = center / stampData.prefabs.Count;
+            stampData.bounds = bounds;
 
             // string jsonString = Serializer.Serialize(stampData, true); // EasyJSON plugin
             string jsonString = JsonUtility.ToJson(stampData, true);
@@ -124,237 +368,5 @@ namespace EastshadeStudio
             }
         }
 
-        [MenuItem("Assets/Stamp")]
-        static void StartStamping()
-        {
-            Init();
-            Object obj = Selection.activeObject;
-            if (obj.GetType() == typeof(TextAsset))
-            {
-                // instance.Stamp = Serializer.Deserialize<StampData>((Selection.activeObject as TextAsset).text); // EasyJson plugin
-                instance.Stamp = JsonUtility.FromJson<StampData>((Selection.activeObject as TextAsset).text);
-            } else
-            { // we will generate a stamp from this single prefab
-                StampData singleStamp = new StampData();
-                StampChild singleChild = new StampChild();
-                singleChild.PrefabPath = AssetDatabase.GetAssetPath(obj);
-                singleStamp.prefabs.Add(singleChild);
-                instance.Stamp = singleStamp;
-            }
-        }
-
-        [MenuItem("Assets/Stamp", true)]
-        static bool StampCheck()
-        {
-            Object candidate = Selection.activeObject;
-
-            if (candidate == null)
-            {
-                return false;
-            } else if (Selection.activeObject.GetType() == typeof(GameObject))
-            {
-                return true;
-            } else if (Selection.activeObject.GetType() == typeof(TextAsset))
-            {
-                try
-                {
-                    // if (Serializer.Deserialize<StampData>((candidate as TextAsset).text) == null) // EasyJson plugin
-                    if (JsonUtility.FromJson((candidate as TextAsset).text, typeof(StampData)) == null)
-                    {
-                        return false;
-                    }
-                }
-                catch
-                {
-                    return false;
-                }
-
-                return true;
-            }
-
-            return false;
-        }
-
-        public static void Init()
-        {
-            if (LayerMask.GetMask("Terrain") > 0)
-            {
-                targetLayer = LayerMask.NameToLayer("Terrain");
-            }
-
-            // Get existing open window or if none, make a new one:
-            PrefabStamper window = (PrefabStamper)GetWindow(typeof(PrefabStamper));
-            window.Show();
-            window.titleContent.text = "PrefabStamper";
-            window.minSize = new Vector2(248, 148);
-            window.maxSize = new Vector2(250, 150);
-            instance = window;
-        }
-
-        void OnEnable()
-        {
-            SceneView.onSceneGUIDelegate += OnScene;
-        }
-        void OnDisable()
-        {
-            SceneView.onSceneGUIDelegate -= OnScene;
-        }
-
-        void OnGUI()
-        {
-            if (instance == null)
-            {
-                instance = (PrefabStamper)GetWindow(typeof(PrefabStamper));
-            }
-
-            targetLayer = EditorGUILayout.LayerField("Target layer", targetLayer);
-
-            useNormal = EditorGUILayout.Toggle("Use Normal", useNormal);
-            randomYRot = EditorGUILayout.Toggle("Randomize Y Rotation", randomYRot);
-
-            sizeVariation = EditorGUILayout.Toggle("Randomize Scale", sizeVariation);
-            if (sizeVariation == true)
-            {
-                EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.LabelField("Smallest/Largest", GUILayout.MaxWidth(120));
-                smallest = EditorGUILayout.FloatField(smallest);
-                largest = EditorGUILayout.FloatField(largest);
-                EditorGUILayout.EndHorizontal();
-            }
-
-            if (GUILayout.Button("Done", GUILayout.MinHeight(30)))
-            {
-                instance.Close();
-            }
-        }
-
-        // main loop
-        static void OnScene(SceneView sceneview)
-        {
-            if (Event.current.alt) // lat key is down
-            {
-                return;
-            }
-
-            if (instance == null)
-            {
-                instance = (PrefabStamper)GetWindow(typeof(PrefabStamper));
-            }
-
-            if (instance.Stamp == null) return;
-
-            if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
-            {
-                LayStamp();
-                Event.current.Use();
-            }
-
-            if (Event.current.type == EventType.MouseUp)
-            {
-                instance.stampedTransforms.Clear();
-            }
-
-            if (instance.stampedTransforms.Count > 0)
-            {
-                UpdateStamped();
-            }
-
-            if (Event.current.type == EventType.Layout)
-            { // stop selecting objects
-                HandleUtility.AddDefaultControl(0);
-            }
-        }
-
-        static void UpdateStamped()
-        {
-            // rotate with scrollwheel
-            float scroll = (Event.current.type == EventType.scrollWheel) ? Event.current.delta.y * 4f : 0f;
-            if (Event.current.type == EventType.scrollWheel)
-            {
-                Event.current.Use();
-            }
-
-            foreach (Transform t in instance.stampedTransforms)
-            {
-                t.RotateAround(instance.lastHitPoint, instance.lastHitNormal, scroll);
-            }
-
-            Vector2 guiPosition = Event.current.mousePosition;
-            Ray ray = HandleUtility.GUIPointToWorldRay(guiPosition);
-
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << targetLayer))
-            {
-                foreach (Transform t in instance.stampedTransforms)
-                {
-                    t.position += hit.point - instance.lastHitPoint;
-                }
-                instance.lastHitPoint = hit.point;
-                instance.lastHitNormal = hit.normal;
-            }
-
-            DropToTerrain(instance.stampedTransforms.ToArray());
-        }
-
-        static int undoGroupIncrementer = 0;
-
-        static void LayStamp()
-        {
-            Undo.IncrementCurrentGroup();
-
-            GameObject newStampsGroup = GameObject.Find("new stamps");
-            if (newStampsGroup == null || newStampsGroup.transform.root != newStampsGroup.transform)
-            {
-                newStampsGroup = new GameObject("new stamps");
-            }
-
-            Vector2 guiPosition = Event.current.mousePosition;
-            Ray ray = HandleUtility.GUIPointToWorldRay(guiPosition);
-
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << targetLayer) == true)
-            {
-                instance.lastHitPoint = hit.point;
-                instance.lastHitNormal = hit.normal;
-                instance.stampedTransforms.Clear();
-
-                foreach (StampChild child in instance.Stamp.prefabs)
-                {
-                    var obj = AssetDatabase.LoadAssetAtPath(child.PrefabPath, typeof(GameObject));
-                    GameObject newGo = PrefabUtility.InstantiatePrefab(obj) as GameObject;
-                    //newGo.name = newGo.name.Split('(')[0];
-                    newGo.transform.parent = newStampsGroup.transform;
-                    newGo.transform.position = (child.position - instance.Stamp.center) + hit.point;
-                    newGo.transform.eulerAngles = child.eulerAngles;
-                    newGo.transform.localScale = child.scale;
-
-                    if (instance.sizeVariation == true)
-                    {
-                        newGo.transform.localScale *= Random.Range(instance.smallest, instance.largest);
-                    }
-
-                    instance.stampedTransforms.Add(newGo.transform);
-                    Undo.RegisterCreatedObjectUndo(newGo, "lay stamp" + undoGroupIncrementer.ToString());
-                }
-
-                undoGroupIncrementer++;
-            }
-        }
-
-        [System.Serializable]
-        class StampData
-        {
-            public Vector3 center = Vector3.zero;
-            public List<StampChild> prefabs = new List<StampChild>();
-        }
-
-        [System.Serializable]
-        class StampChild
-        {
-            public string PrefabPath;
-            public Vector3 position = Vector3.zero;
-            public Vector3 eulerAngles = Vector3.zero;
-            public Vector3 scale = Vector3.one;
-        }
     }
 }
